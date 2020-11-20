454a19e7b3be7265179152873f77bd953c60c387
Giancarlo Abel Giulian <giancarlogiulian@gmail.com>
2020-11-20 14:37:14 -0300

    part 3

--- src/main/java/com/example/demo/WhateverIWant.java
+++ src/main/java/com/example/demo/WhateverIWant.java
@@ -1,18 +1,35 @@
 package com.example.demo;
 
+import java.util.HashSet;
 import java.util.Map;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Set;
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.demo.domain.Player;
+import com.example.demo.domain.Team;
+
+@RestController
 public class WhateverIWant {
 	
-	@RequestMapping("/hi/{name}")
-	public String hiThere( Map model, @PathVariable String name ) {
-		model.put("name", name);
-		return "hello";
+	private Team team;
+	
+	@PostConstruct
+	public void init() {
+		
+		Set<Player> players = new HashSet<>();
+		players.add(new Player("Lebron James", "ala"));
+		players.add(new Player("Michael Jackson", "ala"));
+		
+		team = new Team("Lakers", "Los Angeles", players);
+		
+	}
+	
+	@RequestMapping("/hi")
+	public @ResponseBody Team hiThere( Map model ) {
+		return team;
 	}
 
 }
